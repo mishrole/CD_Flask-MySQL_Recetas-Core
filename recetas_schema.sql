1a1f12b38ce01caae5b91d3fbd58eec4916a1125
Mitchell Rodríguez <mitrodle@gmail.com>
2022-02-04 17:04:25 -0500

    clear: remove sql comments

--- recetas_schema.sql
+++ recetas_schema.sql
@@ -37,17 +37,3 @@ Create table reports (
     foreign key (user_id) references users (id)
 );
 
-/*Create table roles (
-	id int primary key auto_increment,
-    name varchar(45),
-    abbreviation varchar(5)
-);
-
-Insert into roles(name) values ('Administrator', 'Admin'), ('User', 'User');
-
-Create table users_has_roles (
-	user_id int not null,
-    role_id int not null,
-    foreign key (user_id) references users(id),
-    foreign key (role_id) references roles(id)
-);*/
